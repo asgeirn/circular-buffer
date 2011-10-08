f15277faca37bf952b86f55c8bd3709cbaaf84e2
Asgeir Storesund Nilsen <asgeir@twingine.no>
2011-10-08 16:58:17 +0200

    Initial implementation of blocking circular buffer.

---
+++ src/main/java/com/asgeirnilsen/blog/BlockingCircularBuffer.java
@@ -0,0 +1,41 @@
+package com.asgeirnilsen.blog;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class BlockingCircularBuffer<T> extends CircularBuffer<T> {
+
+    private final Lock lock;
+    private final Condition condition;
+
+    public BlockingCircularBuffer(int size, Lock lock) {
+        super(size);
+        this.lock = lock;
+        condition = lock.newCondition();
+    }
+
+    @Override
+    public void add(T item) {
+        super.add(item);
+        condition.signalAll();
+    }
+
+    @Override
+    public T take(AtomicInteger idx) {
+        T result;
+        while ((result = super.take(idx)) == null)
+            try {
+                lock.lock();
+                condition.await();
+            } catch (InterruptedException e) {
+                // ignored
+            } finally {
+                lock.unlock();
+            }
+        return result;
+    }
+
+
+
+}
